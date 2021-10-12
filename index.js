c900ade43453fd15169d9f001e3161730938cb7b
Mark Momot <markmomot03@gmail.com>
2021-10-12 09:18:03 -0400

    commit tree 5ba4e4660c7ef0469e4fd2e28ee25610c2d77360

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+ function saturdayFun(activity = "roller-skate"){
+     return `This Saturday, I want to ${activity}!`;
+ }
+
+ const mondayWork = function(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+ }
+
+ function wrapAdjective(flare = "*"){
+    return function(string = "special"){
+        return `You are ${flare}${string}${flare}!`;
+    }
+ }
+
+ const encouragingPromptFunction = wrapAdjective("!!!");
